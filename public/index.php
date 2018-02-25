714bc8a2bf4fa74ebc71e84128769e979167c14d
jiri.grussmann <cz.georgio@gmail.com>
2018-02-25 14:47:30 +0100

    2

--- public/index.php
+++ public/index.php
@@ -9,36 +9,37 @@
 </head>
 <body>
 <div id="root">
-    <div id="main-header">
-        <div id="page-logo">Comfy<span>Shoppin'</span></div>
-        <div id="cart-status">
-            <div class="ico">
-                <i class="fas fa-shopping-cart"></i>
-                <div class="items-count">0</div>
+    <div class="page-grid">
+        <div id="main-header">
+            <div id="page-logo">Comfy<span>Shoppin'</span></div>
+            <div id="cart-status">
+                <div class="ico">
+                    <i class="fas fa-shopping-cart"></i>
+                    <div class="items-count">0</div>
+                </div>
+                <div class="price">0 <div class="currency">Kč</div></div>
             </div>
-            <div class="price">0 <div class="currency">Kč</div></div>
+        </div>
+
+        <div id="page-cover">
+            <div class="back-cover">Project</div>
+            <h1>Comfortable shopping <span>version 1.0</span></h1>
+        </div>    
+
+        <?php 
+            $json_prods = file_get_contents('data/product-list.json');
+            $prodArray = json_decode($json_prods);
+        ?>
+
+        <div class="products-holder">
+            <?php foreach ($prodArray as $produkt): ?>
+                <div class="product">
+                    <div class="name"><?= htmlspecialchars($produkt->name) ?></div>
+                    <a href="#" class="add-product-to-cart">pridaj</a>
+                </div>        
+            <?php endforeach; ?>
         </div>
     </div>
-
-    <div id="page-cover">
-        <div class="back-cover">Project</div>
-        <h1>Comfortable shopping <span>version 1.0</span></h1>
-    </div>    
-
-    <?php 
-        $json_prods = file_get_contents('data/product-list.json');
-        $prodArray = json_decode($json_prods);
-    ?>
-
-    <div class="products-holder">
-        <?php foreach ($prodArray as $produkt): ?>
-            <div class="product">
-                <div class="name"><?= htmlspecialchars($produkt->name) ?></div>
-                <a href="#" class="add-product-to-cart">pridaj</a>
-            </div>        
-        <?php endforeach; ?>
-    </div>
-
     <shopping-cart></shopping-cart>
 
 </div>
